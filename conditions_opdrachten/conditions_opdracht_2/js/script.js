8257a4c2ffbaef106335d016c1d6cfc99c9e53fb
Daan Broekhuizen <daanbroekhuizen@live.com>
2019-12-03 10:51:43 +0100

    opdracht 2 conditions af

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,9 @@
 //javascript code
+
+var number = Number (prompt("Enter a number:"));
+    if (number < 25) {
+        console.log (number + " euro is niet genoeg om saldo op te nemen");
+    }
+    else {
+        console.log(number + " euro is genoeg om 25 euro op te nemen ");
+    }
